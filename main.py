5a4031343aaef31a38dd47cc71658bc406c992f9
Sinan Abbas <Sinan.Abbas@novigo.com>
2025-07-31 10:35:59 +0530

    optimized

--- main.py
+++ main.py
@@ -30,12 +30,6 @@ async def invoke_model(request: InferenceRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@app.get("/model/{agent}")
-async def get_model_config(agent: str):
-    config = model_map.get(agent)
-    if not config:
-        raise HTTPException(status_code=404, detail="Agent config not fxound")
-    return config
-    
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8007)
+    uvicorn.run(app, host="0.0.0.0", port=7001)
